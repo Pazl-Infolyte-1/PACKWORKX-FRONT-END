37583540af9b7b7cba8587f3f78d8ad98caf6b6e
vignesh1816 <vignesh.s@pazl.info>
2025-03-03 18:14:16 +0530

    Work Order Tables update

---
+++ src/Module/WorkOrder/WorkOrderTable.js
@@ -0,0 +1,89 @@
+import React from 'react'
+import {
+  CTable,
+  CTableRow,
+  CTableHeaderCell,
+  CTableBody,
+  CTableDataCell,
+  CTableHead,
+} from '@coreui/react'
+
+const WorkOrderTable = ({ cellData }) => {
+  return (
+    <div>
+      <div className="max-h-[500px] overflow-y-auto border border-gray-200">
+        <CTable striped hover className="mt-3 w-full">
+          <CTableHead className="bg-gray-100 sticky top-0 z-10">
+            <CTableRow>
+              <CTableHeaderCell className="py-3 px-4 text-gray-600 font-medium">
+                Number
+              </CTableHeaderCell>
+              <CTableHeaderCell className="py-3 px-4 text-gray-600 font-medium">
+                Sales Order
+              </CTableHeaderCell>
+              <CTableHeaderCell className="py-3 px-4 text-gray-600 font-medium">
+                Client
+              </CTableHeaderCell>
+              <CTableHeaderCell className="py-3 px-4 text-gray-600 font-medium">
+                Created Date
+              </CTableHeaderCell>
+              <CTableHeaderCell className="py-3 px-4 text-gray-600 font-medium">
+                ETD
+              </CTableHeaderCell>
+              <CTableHeaderCell className="py-3 px-4 text-gray-600 font-medium">
+                Route(s)
+              </CTableHeaderCell>
+              <CTableHeaderCell className="py-3 px-4 text-gray-600 font-medium">
+                SKU Name
+              </CTableHeaderCell>
+              <CTableHeaderCell className="py-3 px-4 text-gray-600 font-medium">
+                Qty
+              </CTableHeaderCell>
+              <CTableHeaderCell className="py-3 px-4 text-gray-600 font-medium">
+                Stage
+              </CTableHeaderCell>
+              <CTableHeaderCell className="py-3 px-4 text-gray-600 font-medium">
+                Status
+              </CTableHeaderCell>
+            </CTableRow>
+          </CTableHead>
+
+          <CTableBody>
+            {cellData.length > 0 ? (
+              cellData.map((cell, index) => (
+                <CTableRow key={index} className="border-b">
+                  <CTableDataCell className="py-3 px-4 text-gray-700">
+                    {cell.numbers}
+                  </CTableDataCell>
+                  <CTableDataCell className="py-3 px-4 text-gray-700">
+                    {cell.sales_order}
+                  </CTableDataCell>
+                  <CTableDataCell className="py-3 px-4 text-gray-700">{cell.client}</CTableDataCell>
+                  <CTableDataCell className="py-3 px-4 text-gray-700">
+                    {cell.created_date}
+                  </CTableDataCell>
+                  <CTableDataCell className="py-3 px-4 text-gray-700">{cell.etd}</CTableDataCell>
+                  <CTableDataCell className="py-3 px-4 text-gray-700">{cell.routes}</CTableDataCell>
+                  <CTableDataCell className="py-3 px-4 text-gray-700">
+                    {cell.sku_name}
+                  </CTableDataCell>
+                  <CTableDataCell className="py-3 px-4 text-gray-700">{cell.qty}</CTableDataCell>
+                  <CTableDataCell className="py-3 px-4 text-gray-700">{cell.stage}</CTableDataCell>
+                  <CTableDataCell className="py-3 px-4 text-gray-700">{cell.status}</CTableDataCell>
+                </CTableRow>
+              ))
+            ) : (
+              <CTableRow>
+                <CTableDataCell colSpan={10} className="text-center py-3">
+                  No data available
+                </CTableDataCell>
+              </CTableRow>
+            )}
+          </CTableBody>
+        </CTable>
+      </div>
+    </div>
+  )
+}
+
+export default WorkOrderTable

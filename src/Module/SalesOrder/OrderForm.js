521036176f80fc99894dd7a5b2dfca6bf6177dbf
mithlajmatta <mithlajmatta@gmail.com>
2025-04-22 15:35:44 +0530

    search function implemented for client dropdown and sku dropdown

--- src/Module/SalesOrder/OrderForm.js
+++ src/Module/SalesOrder/OrderForm.js
@@ -1,4 +1,4 @@
-import React, { forwardRef, useEffect, useImperativeHandle, useState } from 'react';
+import React, { forwardRef, useEffect, useImperativeHandle, useRef, useState } from 'react';
 import SkuDetails from './SkuDetails'
 import apiMethods from '../../api/config';
 import ActionButton from '../../components/New/ActionButton';
@@ -20,6 +20,38 @@ const OrderForm = forwardRef(({
   const [clients, setClients] = useState([]); // State for client list
   const [skuFormData, setSkuFormData] = useState(null);
   const [localFormData, setLocalFormData] = useState(formData);
+  const [isOpen, setIsOpen] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+  const dropdownRef = useRef(null);
+
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+    console.log("Search term:", e.target.value);
+  };
+
+
+  
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+  
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+  
+  // Handle client selection
+  const selectClient = (clientName) => {
+    // Update form with selected client
+    const event = { target: { name: 'client', value: clientName } };
+    handleInputChange(event);
+    setIsOpen(false);
+  };
   
   useImperativeHandle(ref, () => ({
     getCompleteFormData: {
@@ -71,17 +103,25 @@ const OrderForm = forwardRef(({
   };
 
   useEffect(() => {
-    const fetchClients = async () => {
-      try {
-        const response = await apiMethods.getClients();
-        setClients(response.data); // Assuming response.data contains the client list
-      } catch (error) {
-        console.error("Error fetching clients:", error);
-      }
-    };
-
-    fetchClients();
-  }, []);
+    const delayDebounce = setTimeout(() => {
+      const fetchClients = async () => {
+        try {
+          const params = searchTerm ? { search: searchTerm } : {};
+          const response = await apiMethods.getClients(params);
+          setClients(response.data); // Assuming response.data contains the client list
+        } catch (error) {
+          console.error("Error fetching clients:", error);
+        }
+      };
+  
+      fetchClients();
+    }, 400); // delay of 400ms (adjust as needed)
+  
+    return () => clearTimeout(delayDebounce); // cleanup on next input
+  }, [searchTerm]);
+  
+  
+  
 
   useEffect(() => {
     setConfirmationMethod(localFormData.confirmation);
@@ -212,19 +252,73 @@ const handleToggleChange = () => {
               <label className="text-black font-normal leading-6 mb-2 text-left">
                 Client*
               </label>
-              <select
-                name="client"
-                value={localFormData.client || ""}
-                onChange={handleInputChange}
-                className="w-[500px] h-[40px] px-2 border-[0.8px] border-[#c2c2c2] rounded-md bg-white text-black"
-              >
-                <option value="">Select Client</option>
-                {clients.map((client, index) => (
-                  <option key={index} value={client.company_name}>
-                    {client.company_name}
-                  </option>
-                ))}
-              </select>
+              <div className="relative w-[500px]" ref={dropdownRef}>
+                <div 
+                  className="w-full h-[40px] px-3 border border-gray-300 rounded-md bg-white text-gray-800 flex items-center justify-between cursor-pointer hover:border-[#8167E5] transition-all duration-200"
+                  onClick={() => setIsOpen(!isOpen)}
+                >
+                  <span className=" truncate">
+                    {localFormData.client || "Select Client"}
+                  </span>
+                  <span className="text-gray-500">
+                    {isOpen ? 
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                        <path d="m18 15-6-6-6 6"/>
+                      </svg>
+                      : 
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                        <path d="m6 9 6 6 6-6"/>
+                      </svg>
+                    }
+                  </span>
+                </div>
+
+                {isOpen && (
+                  <div className="absolute w-full mt-1 border border-gray-200 rounded-md bg-white z-10 max-h-[300px] overflow-y-auto shadow-md">
+                    <div className="sticky top-0 bg-white p-2 border-b border-gray-200">
+                      <div className="relative">
+                        <input
+                          type="text"
+                          placeholder="Search clients..."
+                          value={searchTerm}
+                          onChange={handleSearchChange}
+                          className="w-full h-[35px] pl-8 pr-2 border border-gray-200 rounded-md bg-gray-50 text-black focus:outline-none focus:border-[#8167E5] focus:bg-white"
+                          onClick={(e) => e.stopPropagation()}
+                        />
+                        <svg 
+                          xmlns="http://www.w3.org/2000/svg" 
+                          width="16" 
+                          height="16" 
+                          viewBox="0 0 24 24" 
+                          fill="none" 
+                          stroke="currentColor" 
+                          strokeWidth="2" 
+                          strokeLinecap="round" 
+                          strokeLinejoin="round"
+                          className="absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-400"
+                        >
+                          <circle cx="11" cy="11" r="8"/>
+                          <path d="m21 21-4.3-4.3"/>
+                        </svg>
+                      </div>
+                    </div>
+                    
+                    {clients.length > 0 ? (
+                      clients.map((client, index) => (
+                        <div 
+                          key={index} 
+                          className="px-3 py-2 hover:bg-gray-50 cursor-pointer transition-colors duration-150"
+                          onClick={() => selectClient(client.company_name)}
+                        >
+                          {client.company_name}
+                        </div>
+                      ))
+                    ) : (
+                      <div className="px-3 py-2 text-gray-500">No results found</div>
+                    )}
+                  </div>
+                )}
+              </div>
             </div>
 
             {/* Client Period */}
--- src/Module/WorkOrder/WorkOrderDetails.js
+++ src/Module/WorkOrder/WorkOrderDetails.js
@@ -1,103 +1,103 @@
-import React from 'react'
-import PopUp from '../../components/New/PopUp'
-import ActionButton from '../../components/New/ActionButton'
-import apiMethods from '../../api/config'
+  import React from 'react'
+  import PopUp from '../../components/New/PopUp'
+  import ActionButton from '../../components/New/ActionButton'
+  import apiMethods from '../../api/config'
 
-function WorkOrderDetails({ showPopUp, setShowPopUp, cell }) {
-  return (
-    <PopUp
-      visible={showPopUp === cell.id}
-      showCloseButton={true}
-      setVisible={() => setShowPopUp(null)}
-      height={'95vh'}
-      width={'70vw'}
-    >
-      <div className="bg-gray-50 min-h-full">
-        <div className="max-w-6xl mx-auto p-6">
-          {/* Header */}
-          <header className="mb-8">
-            <div className="flex justify-between items-center">
-              <div>
-                <h1 className="text-3xl font-bold text-gray-800">Work Order Details</h1>
+  function WorkOrderDetails({ showPopUp, setShowPopUp, cell }) {
+    return (
+      <PopUp
+        visible={showPopUp === cell.id}
+        showCloseButton={true}
+        setVisible={() => setShowPopUp(null)}
+        height={'95vh'}
+        width={'70vw'}
+      >
+        <div className="bg-gray-50 min-h-full">
+          <div className="max-w-6xl mx-auto p-6">
+            {/* Header */}
+            <header className="mb-8">
+              <div className="flex justify-between items-center">
+                <div>
+                  <h1 className="text-3xl font-bold text-gray-800">Work Order Details</h1>
+                </div>
+                
               </div>
-              
+            </header>
+          </div>
+          <div className="grid grid-cols-1 gap-8">
+            {/* Primary Information Card */}
+            <div className="bg-white rounded-xl shadow-sm overflow-hidden border border-gray-100">
+              <div className="p-6">
+                <h2 className="text-xl font-semibold text-gray-800 mb-4">Work Order Information</h2>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                  <div className="flex flex-col">
+                    <span className="text-sm font-medium text-gray-500">Work Order Name</span>
+                    <span className="text-gray-800 mt-1">{cell.sku_name}</span>
+                  </div>
+                  <div className="flex flex-col">
+                    <span className="text-sm font-medium text-gray-500">Description</span>
+                    <span className="text-gray-800 mt-1">{cell.description}</span>
+                  </div>
+                  <div className="flex flex-col">
+                    <span className="text-sm font-medium text-gray-500">Manufacture</span>
+                    <span className="text-gray-800 mt-1">{cell.manufacture}</span>
+                  </div>
+                  <div className="flex flex-col">
+                    <span className="text-sm font-medium text-gray-500">SKU Version</span>
+                    <span className="text-gray-800 mt-1">{cell.sku_version}</span>
+                  </div>
+                  <div className="flex flex-col">
+                    <span className="text-sm font-medium text-gray-500">Quantity</span>
+                    <span className="text-gray-800 mt-1">{cell.qty}</span>
+                  </div>
+                  <div className="flex flex-col">
+                    <span className="text-sm font-medium text-gray-500">Outsource name</span>
+                    <span className="text-gray-800 mt-1">{cell.outsource_name}</span>
+                  </div>
+                </div>
+              </div>
             </div>
-          </header>
-        </div>
-        <div className="grid grid-cols-1 gap-8">
-          {/* Primary Information Card */}
-          <div className="bg-white rounded-xl shadow-sm overflow-hidden border border-gray-100">
-            <div className="p-6">
-              <h2 className="text-xl font-semibold text-gray-800 mb-4">Work Order Information</h2>
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                <div className="flex flex-col">
-                  <span className="text-sm font-medium text-gray-500">Work Order Name</span>
-                  <span className="text-gray-800 mt-1">{cell.sku_name}</span>
-                </div>
-                <div className="flex flex-col">
-                  <span className="text-sm font-medium text-gray-500">Description</span>
-                  <span className="text-gray-800 mt-1">{cell.description}</span>
-                </div>
-                <div className="flex flex-col">
-                  <span className="text-sm font-medium text-gray-500">Manufacture</span>
-                  <span className="text-gray-800 mt-1">{cell.manufacture}</span>
-                </div>
-                <div className="flex flex-col">
-                  <span className="text-sm font-medium text-gray-500">SKU Version</span>
-                  <span className="text-gray-800 mt-1">{cell.sku_version}</span>
-                </div>
-                <div className="flex flex-col">
-                  <span className="text-sm font-medium text-gray-500">Quantity</span>
-                  <span className="text-gray-800 mt-1">{cell.qty}</span>
-                </div>
-                <div className="flex flex-col">
-                  <span className="text-sm font-medium text-gray-500">Outsource name</span>
-                  <span className="text-gray-800 mt-1">{cell.outsource_name}</span>
+
+            {/* secondary Information Card */}
+            <div className="bg-white rounded-xl shadow-sm overflow-hidden border border-gray-100">
+              <div className="p-6">
+                <h2 className="text-xl font-semibold text-gray-800 mb-4">Secondary Information</h2>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                  <div className="flex flex-col">
+                    <span className="text-sm font-medium text-gray-500">EDD</span>
+                    <span className="text-gray-800 mt-1">{apiMethods.formatDate(cell.edd)}</span>
+                  </div>
+                  <div className="flex flex-col">
+                    <span className="text-sm font-medium text-gray-500">Planned start date</span>
+                    <span className="text-gray-800 mt-1">
+                      {apiMethods.formatDate(cell.planned_start_date)}
+                    </span>
+                  </div>
+                  <div className="flex flex-col">
+                    <span className="text-sm font-medium text-gray-500">Planned end date</span>
+                    <span className="text-gray-800 mt-1">
+                      {apiMethods.formatDate(cell.planned_end_date)}
+                    </span>
+                  </div>
+                  <div className="flex flex-col">
+                    <span className="text-sm font-medium text-gray-500">Created at</span>
+                    <span className="text-gray-800 mt-1">
+                      {apiMethods.formatDate(cell.created_at)}
+                    </span>
+                  </div>
+                  <div className="flex flex-col">
+                    <span className="text-sm font-medium text-gray-500">Updated at</span>
+                    <span className="text-gray-800 mt-1">
+                      {apiMethods.formatDate(cell.updated_at)}
+                    </span>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-
-          {/* secondary Information Card */}
-          <div className="bg-white rounded-xl shadow-sm overflow-hidden border border-gray-100">
-            <div className="p-6">
-              <h2 className="text-xl font-semibold text-gray-800 mb-4">Secondary Information</h2>
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                <div className="flex flex-col">
-                  <span className="text-sm font-medium text-gray-500">EDD</span>
-                  <span className="text-gray-800 mt-1">{apiMethods.formatDate(cell.edd)}</span>
-                </div>
-                <div className="flex flex-col">
-                  <span className="text-sm font-medium text-gray-500">Planned start date</span>
-                  <span className="text-gray-800 mt-1">
-                    {apiMethods.formatDate(cell.planned_start_date)}
-                  </span>
-                </div>
-                <div className="flex flex-col">
-                  <span className="text-sm font-medium text-gray-500">Planned end date</span>
-                  <span className="text-gray-800 mt-1">
-                    {apiMethods.formatDate(cell.planned_end_date)}
-                  </span>
-                </div>
-                <div className="flex flex-col">
-                  <span className="text-sm font-medium text-gray-500">Created at</span>
-                  <span className="text-gray-800 mt-1">
-                    {apiMethods.formatDate(cell.created_at)}
-                  </span>
-                </div>
-                <div className="flex flex-col">
-                  <span className="text-sm font-medium text-gray-500">Updated at</span>
-                  <span className="text-gray-800 mt-1">
-                    {apiMethods.formatDate(cell.updated_at)}
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
-    </PopUp>
-  )
-}
+      </PopUp>
+    )
+  }
 
-export default WorkOrderDetails
+  export default WorkOrderDetails

a63de51407df986e2a7a49eb9f992282e4f8180f
Columbus-12 <columbus.d@pazl.info>
2025-05-05 10:41:23 +0530

    login route issue fix

--- src/App.js
+++ src/App.js
@@ -79,7 +79,6 @@ import './scss/examples.scss'
 
 const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'))
 
-// Pages
 const Login = React.lazy(() => import('./views/pages/login/Login'))
 const Register = React.lazy(() => import('./views/pages/register/Register'))
 const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))

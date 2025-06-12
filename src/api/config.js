4ca793c3195880e7af5270eaf6810f4cd5a139c5
Columbus-12 <columbus.d@pazl.info>
2025-06-12 10:11:50 +0530

    config produciton

---
+++ src/api/config.js
@@ -0,0 +1,48 @@
+import axios from 'axios'
+import { API_BASE_URL } from './constant'
+ 
+const BASE_URL = API_BASE_URL
+const GST_URL = 'http://sheet.gstincheck.co.in/check/9ee24120971acd5c17dc6cad239d99fa'
+ 
+// Create axios instance with default config
+const apiClient = axios.create({
+  baseURL: BASE_URL,
+  timeout: 20000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+ 
+ 
+ 
+// Request interceptor
+apiClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  (error) => {
+    console.error('Request interceptor error:', error)
+    return Promise.reject(error)
+  },
+)
+ 
+// Response interceptor
+apiClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('token')
+      window.location.href = '/login'
+    }
+    return Promise.reject(error)
+  },
+)
+ 
+ 
+ 
+export { apiClient, GST_URL }
+ 

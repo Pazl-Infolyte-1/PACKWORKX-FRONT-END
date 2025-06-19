295cfc32fa55fc34783cfd7399cb4d3bf40c169f
mithlajmt <mithlajmatta@gmail.com>
2025-06-19 18:48:47 +0530

    invoice table completed2

--- src/Module/InvoiceWorkOrder/InvoiceTable.js
+++ src/Module/InvoiceWorkOrder/InvoiceTable.js
@@ -19,8 +19,8 @@ function InvoiceTable({isMiniMised,invoices}) {
 
     const navigate = useNavigate()
   const columns = [
+    { header: 'Invoice ID', field: 'invoice_number',key:'invoice_number' },
     { header: 'Client', field: 'client_name', key:'client_name' },
-    { header: 'Invoice Number', field: 'invoice_number',key:'invoice_number' },
     {
       key: 'payment_status',
       header: 'Payment Status',
@@ -29,10 +29,10 @@ function InvoiceTable({isMiniMised,invoices}) {
         <>
           <span
             className={`px-3 py-1 rounded-md w-24 text-xs font-semibold text-center inline-block -ml-44
-            ${row.payment_status === 'partial' ? 'bg-blue-200 text-blue-900 border border-blue-300' : ''}
-            ${row.payment_status === 'paid' ? 'bg-emerald-200 text-emerald-900 border border-emerald-300' : ''}
-            ${row.payment_status === 'pending' ? 'bg-amber-200 text-amber-900 border border-amber-300' : ''}
-            ${!['partial','paid','pending'].includes(row.payment_status) ? 'bg-gray-200 text-gray-800 border border-gray-300' : ''}
+            ${row.payment_status === 'partial' ? 'bg-blue-100 text-blue-800 border border-blue-200' : ''}
+            ${row.payment_status === 'paid' ? 'bg-green-100 text-green-800 border border-green-200' : ''}
+            ${row.payment_status === 'pending' ? 'bg-yellow-100 text-yellow-800 border border-yellow-200' : ''}
+            ${!['partial','paid','pending'].includes(row.payment_status) ? 'bg-gray-100 text-gray-800 border border-gray-200' : ''}
             `}
           >
             {row.payment_status}
